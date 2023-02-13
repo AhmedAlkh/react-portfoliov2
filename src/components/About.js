b9c20f026c25ee6d0c5f2572afd68bc782b5cd6d
AhmedA <alkhatib_ahmed1995@hotmail.com>
2023-02-13 10:52:07 -0500

    restructured text alignment and paragraphs on about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,12 +9,16 @@ function About() {
         <div>
           <p>
             Welcome to my portfolio! My name is Ahmed Alkhatib and I am a
-            front-end developer who enjoys building websites and applications. I
-            started coding in 2021 and graduated from an accelerated web
+            front-end developer who enjoys building websites and applications.
+          </p>
+          <p>
+            I started coding in 2021 and graduated from an accelerated web
             development course at University of Toronto(SCS) in 2022. Since
-            then, I have been coding independently and improving my skills. Some
-            things I enjoy doing in my spare time include working out, playing
-            video games, watching movies, and listening to music.
+            then, I have been coding independently and improving my skills.
+          </p>
+          <p>
+            Some things I enjoy doing in my spare time include working out,
+            playing video games, watching movies, and listening to music.
           </p>
           <p>
             Check out some of the projects I've built! I am constantly building
